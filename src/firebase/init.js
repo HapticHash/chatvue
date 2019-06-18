470f15f63ff50aa4be680d394db72f4c59fb0505
haptichash <harshitks2203@gmail.com>
2019-06-18 11:45:18 +0530

    added firebase support

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyAhc7BEOuw-pI8IvrBCm4vrHiU5710zSFo",
+authDomain: "chatvue-8c05d.firebaseapp.com",
+databaseURL: "https://chatvue-8c05d.firebaseio.com",
+projectId: "chatvue-8c05d",
+storageBucket: "chatvue-8c05d.appspot.com",
+messagingSenderId: "627660741246",
+appId: "1:627660741246:web:a4fe21b062edf3ac"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebaseApp.firestore();
