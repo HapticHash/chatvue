cdf92d7c8d056cda8f1b775000360efd807c26a1
haptichash <harshitks2203@gmail.com>
2019-06-18 13:39:10 +0530

    added CreateMessage component

---
+++ src/components/CreateMessage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container" style="margin-bottom: 30px">
+        <form @submit.prevent="createMessage">
+            <div class="form-group">
+                <input type="text" name="message" class="form-control" placeholder="Enter message ..." v-model="newMessage">
+                <p class="text-danger" v-if="errorText">{{ errorText }}</p>
+            </div>
+            <button class="btn btn-primary" type="submit" name="action">Submit</button>
+        </form>
+    </div>
+</template>
